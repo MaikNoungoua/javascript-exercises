233b4122271cdb2086aaec54914e9072ff552218
Maik <noungouamaik2023@outlook.com>
2024-03-03 02:48:59 +0000

    sumAll test all passing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function( firstNumber, secondNumber) {
 
+    let sum = 0;
+    if (secondNumber < firstNumber){
+        for (i = secondNumber; i < firstNumber + 1; i++){
+
+            sum += i;
+        }
+        return sum
+    }
+    else if (firstNumber < 0 || secondNumber < 0)
+    {
+        return "ERROR"
+    }
+    else if (typeof(firstNumber) != 'number' || typeof(secondNumber) != 'number'){
+        return "ERROR"
+    }
+
+    else {
+    for (i = firstNumber; i < secondNumber + 1; i++){
+
+        sum += i;
+    }
+    return sum
+}
 };
 
 // Do not edit below this line
