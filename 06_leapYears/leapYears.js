5a343c8548752d0f3a0349dd66b893e76079f9ac
Maik <noungouamaik2023@outlook.com>
2024-04-14 17:44:06 +0100

    all tests passed until temp conversion

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,14 @@
 const leapYears = function(year) {
 
-    if (year % 4 ==0){
-        return true
-    }
-    else if (year % 4 == 0 && year % 100 == 0 && year % 400 != 0 ){
+    if (year % 4 == 0 && year % 100 == 0 && year % 400 != 0 ){
         return false
     }
     else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0 ){
-        return false  
+        return true  
     }  
+    else if (year % 4 == 0){
+        return true 
+    }
     else {
         return false
     }
@@ -18,3 +18,9 @@ const leapYears = function(year) {
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+/*To be consider a leap year we need: 
+
+    - to be divisible by 4, so remainder of euclidian division is 0 
+    - must no be divisible by 100*/
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(args) {
+  let tempInCelsuis, roundedOutput;
+  const denominator = 9;
+  let numerator = (5*args) - 160;
+  tempInCelsuis = numerator / denominator; 
+  roundedOutput = Math.round(tempInCelsuis * 10) / 10;
+  return roundedOutput
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(args) {
+  let tempinFahrenheit, roundedOutput;
+  const denominator = 5; 
+  const toAdd = 32;
+  let numerator = 9 * args; 
+  tempinFahrenheit = (numerator/denominator) + toAdd;
+  roundedOutput = Math.round(tempinFahrenheit * 10) / 10;
+  return roundedOutput
 };
 
 // Do not edit below this line
