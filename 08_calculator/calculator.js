d581fa3591b01f3f32a389e635eef20da94197cd
Maik <noungouamaik2023@outlook.com>
2024-04-17 11:25:41 +0100

    calculator directory passing all the tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,66 @@
-const add = function() {
+const add = function(num1, num2) {
+
+    let sum; 
+    sum = num1 + num2;
+    return sum
+  }
+  
+
+const subtract = function(num1, num2) {
+  let difference;
+
+  difference = num1 - num2;
+  return difference
+
+};
+
+const sum = function(args) {
+  if (args.length  == 0) {
+    let sum = 0; 
+    return sum
+  }
+  else{
+    let total = 0; 
+    for(index = 0; index < args.length; index++){
+      total += args[index];  
+    }
+    return total 
+    
+  }
+  
+  
+
 	
 };
 
-const subtract = function() {
+const multiply = function(args) {
+
+  let multiplication = 1; 
+
+  for (index = 0; index < args.length; index++ ){
+    multiplication *= args[index];
+  }
+  return multiplication
+};
+
+const power = function(num, power) {
+  
+  let result; 
+  result = num ** power;
+  return result
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(args) {
 
-const multiply = function() {
+  let result = 1; 
 
-};
+  for (index = args; index > 0; index--){
 
-const power = function() {
-	
-};
+    result *= index; 
+  }
+  return result
 
-const factorial = function() {
 	
 };
 
